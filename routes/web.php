a2f199445cbab3d0ef4dbd69c48535cf6c88a1ef
harrisongearhart <hgearhart98@gmail.com>
2026-01-21 09:24:55 -0600

    created PagesController to replace the routes with actaul pages. will add the pages in resources/views/pages next

--- routes/web.php
+++ routes/web.php
@@ -13,19 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+/*
 Route::get('/hello', function () {
     //return view('welcome');
     return 'Hello World!';
 });
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is user '.$name.' with an id of '.$id;
+});
+*/
+
+Route::get('/', 'PagesController@index');
 
 Route::get('/about', function(){
     return view('pages.about');
-});
-
-Route::get('/users/{id}/{name}', function($id, $name){
-    return 'This is user '.$name.' with an id of '.$id;
 });
