3a13d77524c7526a7f86194ca22778ba8856f5a8
harrisongearhart <hgearhart98@gmail.com>
2026-01-21 09:05:41 -0600

    included name with id on users page

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,6 @@ Route::get('/about', function(){
     return view('pages.about');
 });
 
-Route::get('/users/{id}', function($id){
-    return 'This is user ' .$id;
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is user '.$name.' with an id of '.$id;
 });
