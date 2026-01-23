e6dc7f9192a8c49cb87972c25b602756030c7534
harrisongearhart <hgearhart98@gmail.com>
2026-01-23 09:24:39 -0600

    delete post implemented

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -108,6 +108,8 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $post = Post::find($id);
+        $post->delete();
+        return redirect('/posts')->with('success', 'Post Deleted');
     }
 }
