237c3d25c1bdb343ad51a2cd098ee920d01d765e
harrisongearhart <hgearhart98@gmail.com>
2026-01-23 13:40:53 -0600

    posts now linked to userid

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
     <hr>
     <small>Written on {{$post->created_at}}</small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn default">Edit</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
 
     {!! Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
         {{ Form::hidden('_method', 'DELETE') }}
